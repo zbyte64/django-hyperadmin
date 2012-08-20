5f4b4d75241836be86a186f3bff451307edf8487
Jason Kraus <jasonk@cukerinteractive.com>
2012-08-20 12:24:42 -0700

    stubbed out jsonp

--- hyperadmin/mediatypes/json.py
+++ hyperadmin/mediatypes/json.py
@@ -32,13 +32,20 @@ class JSON(MediaType):
     def convert_item_form(self, form):
         return self.convert_form(form)
     
-    def serialize(self, content_type, instance=None, errors=None):
+    def get_payload(self, instance=None, errors=None):
         #CONSIDER a better inferface
         if hasattr(self.view, 'get_items_forms'):
             items = [self.convert_item_form(form) for form in self.view.get_items_forms()]
         else:
             items = [self.convert_instance(item) for item in self.view.get_items()]
-        content = json.dumps(items, cls=DjangoJSONEncoder)
+        #TODO errors
+        if instance:
+            return items[0]
+        return items
+    
+    def serialize(self, content_type, instance=None, errors=None):
+        data = self.get_payload(instance=instance, errors=errors)
+        content = json.dumps(data, cls=DjangoJSONEncoder)
         return http.HttpResponse(content, content_type)
     
     def deserialize(self, form_class, instance=None):
@@ -57,4 +64,16 @@ class JSON(MediaType):
 
 BUILTIN_MEDIA_TYPES['application/json'] = JSON
 
+class JSONP(JSON):
+    def get_jsonp_callback(self):
+        #TODO make configurable
+        return self.view.request.GET['callback']
+    
+    def serialize(self, content_type, instance=None, errors=None):
+        data = self.get_payload(instance=instance, errors=errors)
+        content = json.dumps(data, cls=DjangoJSONEncoder)
+        callback = self.get_jsonp_callback()
+        return http.HttpResponse(u'%s(%s)' % (callback, content), content_type)
+
+BUILTIN_MEDIA_TYPES['text/javascript'] = JSONP
 
