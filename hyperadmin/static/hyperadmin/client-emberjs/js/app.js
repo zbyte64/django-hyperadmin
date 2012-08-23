a2959bc3d0f0eb20b117811791a35f166c4cf84c
Jason Kraus <jasonk@cukerinteractive.com>
2012-08-23 16:10:01 -0700

    more work on ember js client, made data bindings and router

--- hyperadmin/static/hyperadmin/client-emberjs/js/app.js
+++ hyperadmin/static/hyperadmin/client-emberjs/js/app.js
@@ -1,17 +1,141 @@
-var App = Em.Application.create();
+var App = Em.Application.create({});
 
-App.handleResponse = function(data, textStatus, jqXHR) {
-    var view_cls = App.ResourceView
-    var view = view_cls.create(data)
-    $('#container').empty()
-    view.appendTo('#container')
-}
+App.ApplicationController = Ember.Controller.extend();
+
+App.ApplicationView = Ember.View.extend({
+    templateName: 'main'
+});
+
+App.Link = Em.Object.extend({
+  url: function() {
+    var url = this.get('href');
+    if (url.charAt(0) != '/') {
+        url = App.resourceController.collection.get('url') + url
+    }
+    return url
+  }.property('href').cacheable(),
+  emberUrl: function() {
+    return '#'+this.get('url');
+  }.property('href').cacheable()
+})
+App.Query = App.Link.extend({})
+App.Item = App.Link.extend({
+  links: Em.ArrayController.create({})
+})
+App.Template = App.Link.extend({})
+App.Error = Em.Object.extend({})
+
+App.resourceController = Em.ObjectController.create({
+    collection: Em.ObjectController.create({ //bind data to this
+        version: null,
+        href: null,
+        url: function() {
+            var url = this.get('href');
+            var end_pos = url.indexOf('?')
+            if (end_pos > -1) {
+                url = url.substr(0, end_pos)
+            }
+            return url
+        }.property('href'),
+        links: Em.ArrayController.create({
+            handleResponse: function(data) {
+                this.set('content', []);
+                for (var i=0; i<data.length; i++) {
+                    var link_data = data[i];
+                    var link = App.Query.create(link_data);
+                    this.pushObject(link);
+                }
+            }
+        }),
+        items: Em.ArrayController.create({
+            handleResponse: function(data) {
+                this.set('content', []);
+                for (var i=0; i<data.length; i++) {
+                    var item_data = data[i];
+                    var item = App.Item.create(item_data);
+                    this.pushObject(item);
+                }
+            }
+        }),
+        queries: Em.ArrayController.create({
+            handleResponse: function(data) {
+                this.set('content', []);
+                for (var i=0; i<data.length; i++) {
+                    var query_data = data[i];
+                    var query = App.Query.create(query_data);
+                    this.pushObject(query);
+                }
+            }
+        }),
+        template: null,
+        error: null,
+        handleResponse: function(data) {
+            this.set('version', data['version']);
+            this.set('href', data['href']);
+            this.links.handleResponse(data['links'])
+            this.items.handleResponse(data['items'])
+            this.queries.handleResponse(data['queries'])
+            if (data['template']) {
+                this.set('template', App.Template.create(data['template']))
+            } else {
+                this.set('template', null)
+            }
+            if (data['error']) {
+                this.set('error', App.Error.create(data['error']))
+            } else {
+                this.set('error', null)
+            }
+        }
+    }), 
+    followLink: function(url) {
+        var settings = $.extend({}, App.requestDefaults, {
+            url: url,
+            success: this.handleResponse
+        })
+        $.ajax(settings)
+    },
+    submitForm: function(form) {
+        var settings = $.extend({}, App.requestDefaults, {
+            url: form.attr('target'),
+            type: form.attr('method') || 'POST',
+            data: form.serialize(),
+            success: this.handleResponse
+        })
+        $.ajax(settings)
+    },
+    handleResponse: function(data, textStatus, jqXHR) {
+        App.resourceController.collection.handleResponse(data['collection'])
+    }
+});
+
+
+App.Router = Em.Router.extend({
+  location: 'hash',
+  root: Em.Route.extend({
+    index: Em.Route.extend({
+      route: '/',
+      connectOutlets: function(router) {
+        router.get('applicationController').connectOutlet('Resource');
+      },
+      followLink: function(router, event) {
+        var view = event.view
+        var url = view.bindingContext.get('url')
+        App.resourceController.followLink(url)
+      },
+      submitForm: function(router, event) {
+        var view = event.view
+        var form = view.$().find('form');
+        App.resourceController.submitForm(form)
+      }
+    })
+  })
+});
 
 App.handleResponseError = function(jqXHR, textStatus, errorThrown) {
     if (jqXHR.status == 401) {
         var login_url = jqXHR.getResponseHeader("Location")
         if (login_url) {
-            App.followLink(login_url)
+            App.resourceController.followLink(login_url)
         } else {
             console.log([jqXHR, textStatus, errorThrown])
         }
@@ -24,7 +148,7 @@ App.requestDefaults = {
     accepts: {
         'json': 'application/vnd.Collection.next+JSON',
     },
-    success: App.handleResponse,
+    success: App.resourceController.handleResponse,
     error: App.handleResponseError,
     dataType: "json",
     beforeSend: function(jqXHR, settings) {
@@ -55,72 +179,55 @@ App.requestDefaults = {
     }
 }
 
-App.followLink = function(url) {
-    var settings = $.extend({}, App.requestDefaults, {url: url})
-    $.ajax(settings)
-}
-
-App.init_application = function() {
-  var url = $('body').attr('data-api-endpoint')
-  var settings = $.extend({}, App.requestDefaults, {url: url})
-  $.ajax(settings)
-}
-
-App.AdminView = Em.View.extend({
-    followLink: function(event) {
-        var view = event.view
-        var url = view.bindingContext.href
-        App.followLink(url)
-    },
-    submitForm: function(event) {
-        var view = event.view
-        var $form = view.$().find('form');
-        var settings = $.extend({}, App.requestDefaults, {
-            url: $form.attr('target'),
-            type: $form.attr('method') || 'POST',
-            data: $form.serialize()
-        })
-        $.ajax(settings)
-    }
-})
-
-App.LoadingView = App.AdminView.extend({
-
-});
+App.AdminView = Em.View.extend({})
 
 App.ResourceView = App.AdminView.extend({
-  templateName: 'resource'
+  templateName: 'resource',
+  classNames: ['resource']
 })
 
 App.BreadcrumbsView = App.AdminView.extend({
-  templateName: 'breadcrumbs'
+  templateName: 'breadcrumbs',
+  classNames: ['breadcrumbs']
 })
 
 App.QueriesView = App.AdminView.extend({
-  templateName: 'queries'
+  templateName: 'queries',
+  classNames: ['queries']
 })
 
 App.ErrorView = App.AdminView.extend({
-  templateName: 'error'
+  templateName: 'error',
+  classNames: ['error']
 })
 
 App.ItemsView = App.AdminView.extend({
-  templateName: 'items'
+  templateName: 'items',
+  classNames: ['items']
 })
 
 App.TemplateView = App.AdminView.extend({
-  templateName: 'template'
+  templateName: 'template',
+  classNames: ['template']
 })
 
 App.LinkView = App.AdminView.extend({
-  templateName: 'link'
+  templateName: 'link',
+  classNames: ['link']
 })
 
-App.BreadcrumbView = App.LinkView.extend({
-
-})
+App.BreadcrumbView = App.LinkView.extend({})
 
 App.FormView = App.AdminView.extend({
-  templateName: 'form'
+  templateName: 'form',
+  classNames: ['form']
 })
+
+App.AplicationView = App.ResourceView;
+
+$(function() {
+  App.initialize();
+  var url = $('body').attr('data-api-endpoint')
+  App.resourceController.followLink(url)
+});
 
