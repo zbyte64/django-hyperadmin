b3a294d753fd0e70ca620a1a02b93c16d1ae9bb8
Jason Kraus <jasonk@cukerinteractive.com>
2012-10-09 16:34:53 -0700

    started on iframe transport media type

--- hyperadmin/mediatypes/__init__.py
+++ hyperadmin/mediatypes/__init__.py
@@ -2,3 +2,4 @@ from common import MediaType, BUILTIN_MEDIA_TYPES
 import collectionjson
 import html5
 import json
+import iframe
---
+++ hyperadmin/mediatypes/iframe.py
@@ -0,0 +1,69 @@
+import mimeparse
+
+from django.template.response import TemplateResponse
+from django.middleware.csrf import CsrfViewMiddleware
+
+from common import MediaType
+
+
+class IframeMediaType(MediaType):
+    template_name = 'hyperadmin/iframe/resource.html'
+    response_class = TemplateResponse
+    recognized_media_types = [
+        'text/html-iframe-transport',#;app/yourtype
+    ]
+    
+    def get_response_type(self):
+        response_type = self.view.get_response_type(patch_meta=True)
+        effective_type = response_type.split(',',1)[-1]
+        return mimeparse.best_match(
+            self.site.media_types.keys(),
+            effective_type
+        )
+    
+    def get_response_media_type(self):
+        content_type = self.get_response_type()
+        media_type_cls = self.site.media_types.get(content_type, None)
+        if media_type_cls is None:
+            raise ValueError('Unrecognized response content type: %s' % content_type)
+        return media_type_cls(self.view)
+    
+    def get_context_data(self, link, state):
+        media_type = self.get_response_media_type()
+        content_type = self.get_response_type()
+        response = media_type.serialize(content_type=content_type, link=link, state=state)
+        context = {
+            'payload':response.content,
+            'content_type': content_type,
+        }
+        return context
+    
+    def get_template_names(self):
+        return [self.template_name]
+    
+    def serialize(self, content_type, link, state):
+        if self.detect_redirect(link):
+            return self.handle_redirect(link)
+        
+        print 'iframe handler'
+        
+        context = self.get_context_data(link=link, state=state)
+        response = self.response_class(request=self.request, template=self.get_template_names(), context=context)
+        response['Content-Type'] = 'text/html'
+        return response
+    
+    def deserialize(self):
+        self.check_csrf()
+        
+        return {'data':self.request.POST,
+                'files':self.request.FILES,}
+    
+    def check_csrf(self):
+        csrf_middleware = CsrfViewMiddleware()
+        response = csrf_middleware.process_view(self.view.request, self.deserialize, self.view.args, self.view.kwargs)
+        if response is not None:
+            assert False, 'csrf failed' #TODO APIException(response) or SuspiciousOperation ....
+            raise response
+
+IframeMediaType.register_with_builtins()
+
