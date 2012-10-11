5ccfadc937c51d6c7c8c8e5d07afa6247a61ced8
Jason Kraus <jasonk@cukerinteractive.com>
2012-10-10 17:26:22 -0700

    fixed url settings for docproject

--- docs/source/docproject/settings.py
+++ docs/source/docproject/settings.py
@@ -102,7 +102,7 @@ MIDDLEWARE_CLASSES = (
     'django.contrib.messages.middleware.MessageMiddleware',
 )
 
-ROOT_URLCONF = 'tests.urls'
+ROOT_URLCONF = 'docproject.urls'
 
 TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
